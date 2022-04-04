f2f5637a91658d710d20e23c483d4edd28461cae
Aoliarachman <aoliarachman150@gmail.com>
2022-04-04 22:12:28 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Aoliarachman/go-say-hello
+
+go 1.17
---
+++ say_hello.go
@@ -0,0 +1,9 @@
+package go_say_hellogit
+
+func sayHello(nama string) string {
+	return "Hello" + nama
+}
+
+func kali(a int, b int) int {
+	return a * b
+}
